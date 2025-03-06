6c1f65683c30e70182983a3bd422cae7d395339f
Bishal Paul <123397462+Bpbishal@users.noreply.github.com>
2025-03-06 20:42:02 +0600

    Add files via upload

---
+++ Advanced WT/Mid/Lab/abd.js
@@ -0,0 +1,9 @@
+class a{
+    constructor(name)
+    {
+        this.name=name;
+
+    }
+}
+const ab=new a("Bishal")
+console.log(ab.name);
---
+++ Advanced WT/Mid/Lab/try.js
@@ -0,0 +1,17 @@
+const a="Bishal"
+console.log(`Hello ${a}`);
+
+const aa=(name="Bp")=>
+    `Hello ${name}`
+console.log(aa());
+
+class ab{
+    sayHello()
+    {
+        setTimeout(()=>{
+         console.log(why);
+         
+        },1000);
+    }
+}
+const abn=new ab;
