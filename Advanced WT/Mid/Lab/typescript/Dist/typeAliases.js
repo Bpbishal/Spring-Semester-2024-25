afb264b6cf342846fb66b043057b21bb421c75d7
Bpbishal <bishalpal816@gmail.com>
2025-04-17 03:19:06 +0600

    ts type

---
+++ Advanced WT/Mid/Lab/typescript/Dist/typeAliases.js
@@ -0,0 +1,4 @@
+"use strict";
+let userId = 2;
+console.log(typeof (userId));
+//# sourceMappingURL=typeAliases.js.map
---
+++ Advanced WT/Mid/Lab/typescript/src/typeAliases.ts
@@ -0,0 +1,3 @@
+type id=number|string
+let userId:id=2
+console.log(typeof(userId))
