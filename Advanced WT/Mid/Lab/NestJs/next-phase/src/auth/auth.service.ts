9696f09623a8cb9833c83faf2dfdc1344e338c78
Bpbishal <bishalpal816@gmail.com>
2025-04-29 22:18:19 +0600

    29.04.25

--- Advanced WT/Mid/Lab/NestJs/next-phase/src/auth/auth.service.ts
+++ Advanced WT/Mid/Lab/NestJs/next-phase/src/auth/auth.service.ts
@@ -26,24 +26,29 @@ export class AuthService {
     const { email, password, role } = signUpDto;
   
     const hashedPassword = await bcrypt.hash(password, 10);
+  
     const user = this.userRepository.create({ 
       email, 
       password: hashedPassword, 
-      role: role as 'admin' | 'HR' | 'employee' | 'department_manager' 
+      role: role as 'employee' | 'admin' | 'HR' | 'department_manager',
     });
   
     if (role === 'employee') {
       const employee = this.employeeRepository.create({
         name: 'New Employee',
-        email: email,  
-        role: role, 
+        email: email,
+        role: role,
         department: 'Unknown',
       });
+  
+      await this.employeeRepository.save(employee);
+  
       user.employee = employee;
     }
   
     return this.userRepository.save(user);
   }
+  
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id, role: user.role };
--- Advanced WT/Mid/Lab/NestJs/next-phase/src/auth/dto/signup.dto.ts
+++ Advanced WT/Mid/Lab/NestJs/next-phase/src/auth/dto/signup.dto.ts
@@ -2,13 +2,13 @@ import { isEmail, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class SignupDto {
   @IsNotEmpty()
-  @MinLength(3)
+  //@MinLength(3)
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
   @MinLength(6)
   password: string;
-  
+  @IsNotEmpty()
   role: string; 
 }
